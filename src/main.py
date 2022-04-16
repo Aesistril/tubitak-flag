5c79b4464d20d9b6e2aab3da1087d9dabf7aae33
Aesistril <yigitayaz262@gmail.com>
2022-04-17 02:03:49 +0300

    added the region game config meu

--- src/main.py
+++ src/main.py
@@ -31,6 +31,10 @@ class select_lang(QtWidgets.QWidget):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent=parent)
         uic.loadUi(dirs["resources"]["qt6-ui"].replace("-filepath-", os.dirname(__file__))+'/select_lang.ui', self)
+class region_game_config(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent=parent)
+        uic.loadUi(dirs["resources"]["qt6-ui"].replace("-filepath-", os.dirname(__file__))+'/region_game_config.ui', self)
 
 # Select game widget (The main menu)
 class select_game(QtWidgets.QWidget):
@@ -55,29 +59,45 @@ class MainWin(QtWidgets.QMainWindow):
     def changeLang(self):
         self.lang = ConfigParser()
         # Read the comboBox and read the config file of language user has selected
-        test = dirs["resources"]["lang"] + "/" + self.select_lang.comboBox.currentText() + ".conf"
         self.lang.read(dirs["resources"]["lang"].replace("-filepath-", os.dirname(__file__)) + "/" + self.select_lang.comboBox.currentText() + ".conf")
         del self.select_lang
-        self.setWindowTitle(lang["misc"]["window_title"]) # Change the window title
+        self.setWindowTitle(self.lang["misc"]["window_title"]) # Change the window title
         self.draw_select_game()
 
     def draw_select_game(self):
+        # Delete the old self.select_game if possible
+        try: del self.select_game
+        except: pass
         # Draw the main menu
         self.select_game = select_game()
-        self.setCentralWidget(self.select_game)
         self.select_game.sel_game_label.setText(self.lang["sel_game"]["sel_game_label"])
         self.select_game.region_game.setText(self.lang["sel_game"]["region_game"])
         self.select_game.country_game.setText(self.lang["sel_game"]["country_game"])
-        self.select_game.region_game.clicked.connect(self.region_game)
-        self.select_game.country_game.clicked.connect(self.country_game)
+        self.select_game.region_game.clicked.connect(self.draw_region_game_config)
+        self.select_game.country_game.clicked.connect(self.draw_country_game)
+        self.setCentralWidget(self.select_game)
         self.setFixedSize(640, 450)
     
-    def country_game(self):
+    def draw_country_game(self):
         pass
 
-    def region_game(self):
+    def draw_region_game_config(self):
+        # Delete the old self.region_game_config if possible
+        try: del self.region_game_config
+        except: pass
+        self.region_game_config = region_game_config()
+        self.region_game_config.choice_count_label.setText(self.lang["region_game_config"]["choice_count_label"])
+        self.region_game_config.q_count_label.setText(self.lang["region_game_config"]["q_count_label"])
+        self.region_game_config.title.setText(self.lang["region_game_config"]["title"])
+        self.region_game_config.start.setText(self.lang["region_game_config"]["start"])
+        self.region_game_config.return_main.setText(self.lang["region_game_config"]["return_main"])
+        self.setCentralWidget(self.region_game_config)
+
+        self.region_game_config.return_main.clicked.connect(self.draw_select_game)
+        self.region_game_config.start.clicked.connect(self.run_region_game)
+    
+    def run_region_game(self):
         pass
-        
 
 # Start the application
 if __name__ == '__main__':
